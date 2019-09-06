dc71ef1f80bf10b79ac855273e22189b606a02bb
wololoaoe <49536906+wololoaoe@users.noreply.github.com>
2019-09-06 15:06:27 +0200

    Add files via upload

--- js/random.js
+++ js/random.js
@@ -4,7 +4,7 @@ var tmpvalue=0;
 setInterval(function(){
 	value=document.getElementById('number').getAttribute('value');
 	do{
-		tmpvalue=Math.floor((Math.random() * 20) +15);//25+1
+		tmpvalue=Math.floor((Math.random() * 10)+5);//25+1
 	}while(value===tmpvalue);
 	document.getElementById("number").setAttribute("value", tmpvalue);
 },1000);
